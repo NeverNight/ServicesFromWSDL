8b99a136ebc8758008018589d739c0c3be552cec
alex da franca <alex@farbflash.de>
2017-05-28 11:13:21 +0200

    Initial Commit

---
+++ ServicesFromWSDL/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ServicesFromWSDL
+//
+//  Created by Alex Apprime on 28.05.17.
+//  Copyright © 2017 Farbflash. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
